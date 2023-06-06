21774ea2df51cc949d01d5c74fa65be10dd82369
Jerome Goh <jerome.goh.jg@gmail.com>
2023-06-07 00:32:47 +0800

    2) added sign in sign out change dep on auth

--- src/components/compiledData/Navbar.js
+++ src/components/compiledData/Navbar.js
@@ -1,21 +1,37 @@
 import "./Navbar.css";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import {Link, NavLink} from 'react-router-dom';
-import CartIcon from '../mini_components/CartIcon';
 import homePageIcon from '../../images/logo.jpg';
 import Button from '@mui/material/Button';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../config/firebase.js"
 
+
+// need to account for the difference in the navbar when the user is logged in or logged out 
+
+// added in the functionality where the 
 function Navbar() {
-    const [isClicked, setClick] = useState("BUY");
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    // delays the evaluation by wrapping it around a "supplier"
+    const signout = () => {auth.signOut()};
+
+    // don't understand why the useEffect hook would unmount have to unmount 
+    // when do we know what we should include at the second argument of the useEffect hook
+    useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+        setIsLoggedIn(user !== null);
+      });
+
+    return () => unsubscribe();
+    }, []);
 
     const actionArr = [
         {title: 'BUY'},
         {title: 'SELL'},
         {title: 'TRADE'}
     ]
-    const clickHandler = () => {
-        setClick()
-    }
+
 
     return (
         <div> 
@@ -28,15 +44,19 @@ function Navbar() {
         {actionArr.map((action, index)=> {
         return (
         <NavLink activeClassName = "active" to = {`/${action.title}`}>
-            <button ifClick = {clickHandler} className = "main_button" key = {index}>
+            <button className = "main_button" key = {index}>
             {action.title}
             </button>
         </NavLink>)})}
 
-        <Link to = "/SIGNUP" className = "sign-up" reloadDocument> 
-            <Button size = "large" variant = "text" sx = {{color: "black", borderColor: "black"}}> SIGN IN </Button>
-        </Link>
-
+        {isLoggedIn ? 
+            <Link to = "/SIGNUP" className = "sign-up" reloadDocument> 
+            <Button onClick =  {signout} size = "large" variant = "text" sx = {{color: "black", borderColor: "black"}}> SIGN OUT </Button>
+            </Link> :
+            <Link to = "/SIGNUP" className = "sign-up" reloadDocument> 
+            <Button  size = "large" variant = "text" sx = {{color: "black", borderColor: "black"}}> SIGN IN </Button>
+            </Link> 
+        }
         <Link to = "/BUY/CART" className = "cart">
             <Button size = "large" variant = "text" sx = {{color: "black", borderColor: "black"}}> CART </Button>
         </Link> 
