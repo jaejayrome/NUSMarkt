5fc8c9428094301e13dd5977579344e8c7cc4308
Jerome Goh <jerome.goh.jg@gmail.com>
2023-06-12 23:00:11 +0800

    SELL: Step 2 Fixed except dimension of box

--- src/components/pages/sell/Sell_addListing2.js
+++ src/components/pages/sell/Sell_addListing2.js
@@ -1,19 +1,15 @@
-import {useState, useRef} from 'react'
-import { getStorage, ref, uploadBytes, getDownloadURL, list} from "firebase/storage";
+import { useState, useRef } from 'react';
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { Box, Button } from '@mui/material';
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 import Navbar from '../../compiledData/Navbar.js';
 import { useLocation, useNavigate } from 'react-router-dom';
 import TransitionModal from '../../mini_components/TransitionModal.js';
 
-// Component have to handle the uploading of the image to the datbase 
 export default function Sell_addListing2() {
-
-  // retrieving the location object 
   const location = useLocation();
   const navigate = useNavigate();
-
-  const listingTitle = location.state.listingTitle
+  const listingTitle = location.state.listingTitle;
 
   const newListing = {
     listingTitle: location.state.listingTitle, 
@@ -23,107 +19,153 @@ export default function Sell_addListing2() {
     filePath: location.state.listingTitle,
     listedBy: location.state.listedBy,
     sizingGuide: location.state.sizingGuide
-}
-
+  };
 
   const navigationHandler = () => {
-    navigate('/SELL/ADD_LISTING/STEP3', {state: newListing})
-}   
-  
+    navigate('/SELL/ADD_LISTING/STEP3', { state: newListing });
+  };
 
-    // this component handles the image upload
-    const [selectedImage, setSelectedImage] = useState(null);
-    const [imageURL, setImageURL] = useState("")
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageURL, setImageURL] = useState("");
+  const imageUploadRef = useRef(null);
 
-    // construct a reference to the file 
-    const imageUploadRef = useRef(null)
+  const imageRefHandler = () => {
+    imageUploadRef.current.click();
+  };
 
-
-     // reference to the hidden input element
-     const imageRefHandler = () => {
-        imageUploadRef.current.click();
-    }
-
-    // need to provide a function to upload another image 
-    const handleImageUpload = async (event) => {
-        const file = event.target.files[0];
+  const handleImageUpload = async (event) => {
+    const file = event.target.files[0];
       
-        try {
-          // creating a reference to the storage
-          const storage = getStorage()
-
-          // creating a filePathReference to the a temporary folder
-          const listingImagesRef = ref(storage, `images/${listingTitle}.jpg`);
-          const snapshot = await uploadBytes(listingImagesRef, file);
-          
-          // Get the URL of the uploaded file
-          const downloadURL = await getDownloadURL(snapshot.ref);
+    try {
+      const storage = getStorage();
+      const listingImagesRef = ref(storage, `images/${listingTitle}.jpg`);
+      const snapshot = await uploadBytes(listingImagesRef, file);
+      const downloadURL = await getDownloadURL(snapshot.ref);
         
-          setSelectedImage(file);
-          setImageURL(downloadURL);
+      setSelectedImage(file);
+      setImageURL(downloadURL);
       
-          console.log('Selected file:', file);
-          console.log('Image URL:', downloadURL);
-        } catch (error) {
-          console.log('Error uploading image:', error);
-        }
-      };
+      console.log('Selected file:', file);
+      console.log('Image URL:', downloadURL);
+    } catch (error) {
+      console.log('Error uploading image:', error);
+    }
+  };
 
-    
-    return (
-        <div>
-            <Navbar />
-            <div style = {{ fontFamily: 'serif', fontWeight: 'bolder', fontSize: "30px", marginBottom: "5%"}}> 
-              Step 2: Upload Your Image
-            </div>
+  return (
+    <div>
+      <Navbar />
 
-              <label htmlFor= 'upload-image'> 
-                              {!selectedImage ? <Button startIcon = {<AddPhotoAlternateOutlinedIcon/>} variant = "outlined" 
-                              sx = {{fontWeight: 'bold', borderColor: 'black', color: 'black', 
-                                      textTransform: "none", 
-                                      '&:hover': {
-                                      backgroundColor: '#D3D3D3',
-                                      borderColor: 'black'
-                                      }}}
-                              onClick = {imageRefHandler}> Upload Image </Button> : 
-                              <Button startIcon = {<AddPhotoAlternateOutlinedIcon/>} variant = "outlined" 
-                              sx = {{fontWeight: 'bold', borderColor: 'black', color: 'black', 
-                                      textTransform: "none", 
-                                      '&:hover': {
-                                      backgroundColor: '#D3D3D3',
-                                      borderColor: 'black'
-                                      }}}
-                              onClick = {imageRefHandler}> Choose Another Image </Button>}
-                </label>
+ 
+      <div style={{ marginLeft: "3%", fontFamily: 'serif', fontWeight: 'bolder', fontSize: "30px" }}> 
+        Step 2: Upload Your Image
+      </div>
 
-                <Box  sx = {{display: "flex",
-                                    flexDirection: "column", justifyContent: 'center', 
-                                    alignItems: 'center',border: '1px dashed black',
-                                    backgroundColor: "#E5E4E2", width: "100%", height: "100%" , 
-                                    '&:hover': {
-                                    backgroundColor: '#D3D3D3',
-                                    opacity: [0.9, 0.8, 0.7]},
-                                    }}>
-                            
-                            <input
-                                accept="image/*"
-                                id="upload-image"
-                                type="file"
-                                ref = {imageUploadRef}
-                                style={{ display: 'none' }}
-                                onChange={handleImageUpload} />
+      <div style = {{display: 'flex', justifyContent: "center"}}>
 
-                            {selectedImage && (
-                                  <div>
-                                    <img
-                                      src={imageURL}
-                                      alt="Image Upload Fail"
-                                      style={{ width: "100%", height: "100%", objectFit: "cover" }}
-                                    />
-                                  </div>)}
-                  </Box>
-                  <TransitionModal navigation = {navigationHandler}/>
+      <div style={{ flex: 1.5, textAlign: "center", marginBottom: "1rem", marginTop: "12rem  "}}>
+        {!selectedImage ? (
+          <Button
+            startIcon={<AddPhotoAlternateOutlinedIcon />}
+            onClick={imageRefHandler}
+            variant="outlined"
+            sx={{
+              fontWeight: "bold",
+              borderColor: "black",
+              color: "black",
+              textTransform: "none",
+              '&:hover': {
+                backgroundColor: '#D3D3D3',
+                borderColor: 'black'
+              },
+            }}
+          >
+            Upload Image
+          </Button>
+        ) : (
+          <Button
+            startIcon={<AddPhotoAlternateOutlinedIcon />}
+            variant="outlined"
+            sx={{
+              fontWeight: "bold",
+              borderColor: "black",
+              color: "black",
+              textTransform: "none",
+              '&:hover': {
+                backgroundColor: '#D3D3D3',
+                borderColor: 'black'
+              },
+            }}
+          >
+            Choose Another Image
+          </Button>
+        )}
+
+        <div style={{ textAlign: "center", marginTop: "1rem" }}>
+        <TransitionModal navigation={navigationHandler} />
         </div>
-    )
+      </div>
 
+      <div style = {{flex: 2, marginTop: "2rem"}}>
+        <Box
+          sx={{
+            border: "1px dashed black",
+            backgroundColor: "#E5E4E2",
+            position: "absolute",
+            width: '50%',
+            height: '65%',
+            margin: '0 auto',
+            "&:hover": {
+              backgroundColor: "#D3D3D3",
+              opacity: [0.9, 0.8, 0.7],
+            },
+          }}
+        >
+          <input
+            accept="image/*"
+            id="upload-image"
+            type="file"
+            ref={imageUploadRef}
+            style={{ display: "none" }}
+            onChange={handleImageUpload}
+          />
+
+          {!selectedImage && (
+            <div
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <span>No Image Found</span>
+            </div>
+          )}
+
+          {selectedImage && (
+            <img
+              src={imageURL}
+              alt="Image Upload Fail"
+              style={{
+                width: "100%",
+                height: "100%"
+              }}
+            />
+          )}
+
+        </Box>
+      </div>
+
+      
+
+      </div>
+
+      
+    </div>
+  );
 }
