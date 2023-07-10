b977bc937776e05cbf140f92a996689ec3e65893
Jerome Goh <jerome.goh.jg@gmail.com>
2023-07-10 18:01:20 +0800

    BACKEND: REROUTE AFTER PAYMENT

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser")
 require("dotenv").config()
 app.use(cors())
 const port = process.env.port
+const successPage = process.env.success_page
+const failedPage = process.env.failed_page
 const Stripe = require("stripe")(process.env.stripe_sk)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -13,14 +15,32 @@ app.get("/", (req, res)=> {
     res.send("Hello World")
 })
 
+
 app.post("/pay", async (req, res) => {
-    console.log(req.body.token)
-    await Stripe.charges.create({
+    // console.log(req.body.token)
+
+    try {
+    const charge = await Stripe.charges.create({
         source: req.body.token.id,
         amount: req.body.amount,
         currency: "sgd",
-    }   
-    )
+    })
+    // upon succesful confirmation, redirect to the frontend 
+    // Handle the charge result based on the actual structure of the charge object
+    console.log(charge);
+    if (charge.status === "succeeded") {
+        // Payment was successful
+        res.json({ success: true });
+    } else {
+        // Payment failed or the status is not available
+        res.json({ success: false });
+    }
+    } catch(error) {
+        console.log(error)
+    res.json({success: false})
+    } 
+    
+
 })
 
 app.listen(port, () => {
