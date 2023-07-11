cd4715c4ee5fd7713e9e3401041e742cd4b83413
Jerome Goh <jerome.goh.jg@gmail.com>
2023-07-12 00:56:20 +0800

    KICKSTARTIT: pre-order listing form made

--- frontend/src/components/pages/sell/Sell_kickStartIt.js
+++ frontend/src/components/pages/sell/Sell_kickStartIt.js
@@ -1,13 +1,120 @@
 import Navbar from "../../compiledData/Navbar";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import db from "../../../config/firebase.js"
+import {auth} from "../../../config/firebase.js"
+import { Divider, TextField, InputLabel, Button, InputAdornment, OutlinedInput, FormControl} from "@mui/material";
+import { addDoc, collection } from "@firebase/firestore";
 
 // NOTE THAT THE NAME OF THE FUNCTION IS NOT THE SAME AS THAT OF THE FILE NAME
+
+// i need to createa a pre-order listing that would then link to another pre-order marketplace
+
+// hence buy would split into listing marketplace, pre-order marketplace, orders placed
 export default function Sell_KickStartIt() {
+
+    const location = useLocation()
+
+    const url = location.state?.url
+    const [title, setTitle] = useState("")
+    const [price, setPrice] = useState("")
+    const [description, setDescription] = useState("")
+    const [target, setTarget] = useState(0)
+
+    const titleHandler = (event) => {
+        setTitle(event.target.value)
+    } 
+
+    const priceHandler = (event) => {
+        setPrice(event.target.value)
+    } 
+
+    const descriptionHandler = (event) => {
+        setDescription(event.target.value)
+    } 
+
+    const targetHandler = (event) => {
+        setTarget(event.target.value)
+    } 
+
+    // const titleHandler = (event) => {
+    //     setTitle(event.target.value)
+    // } 
+
+    const uploadOrder = async () => {
+
+
+        const preOrder = {
+            listingTitle: title, 
+            listingPrice: price, 
+            listingDescription: description,
+            pledgeTarget: target, 
+            pledgeCounter: 0,
+            listedBy: auth.currentUser.displayName, 
+            url: url
+        }
+
+        try {
+        const collectionRef = collection(db, "preOrders")
+        await addDoc(collectionRef, preOrder)
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div> 
             <Navbar />
 
             <div> 
-                Kick Start It!
+                <div> 
+                <div style={{marginBottom: "3%", textAlign: "center"}}> 
+                <div style = {{fontSize: "30px", fontWeight: "bold"}}> 
+                <div> 
+                {/* <CheckCircleIcon size = "large"/> */}
+                </div>
+                Kick Start It
+                </div>
+
+                <div> 
+                Create Your Pre-order Listing by filling the necessary details as follows:
+                </div>
+
+                </div>
+
+                </div> 
+
+                <Divider sx = {{border: "1px solid black", mb: "5%"}}/>
+                <div style={{display: "flex", flexDirection: 'row'}}> 
+
+                <div style={{marginLeft: "10%" , flex: 1}}> 
+                {url && <img height = "400px" width = "400px" src = {url}  alt = "image not found"/>}
+                </div>
+
+                <div style={{flex: 1, display: 'flex', flexDirection: 'column'}}> 
+
+                <TextField onChange = {titleHandler} sx = {{width:  '80%' }} label = "Listing Title" multiline = {false}> </TextField>
+
+                <InputLabel htmlFor = "pricing" sx = {{marginTop:"5%"}}> Proposed Pricing</InputLabel>
+                <OutlinedInput onChange = {priceHandler} startAdornment = {<InputAdornment position="start"> $</InputAdornment>} sx = {{width:  '80%' }} id = "pricing" multiline = {false}> 
+    
+                </OutlinedInput>
+
+                <TextField onChange = {descriptionHandler}  sx = {{width:  '80%', marginTop: "5%" }} label = "Listing Description" multiline = {true}> </TextField>
+
+                <TextField onChange = {targetHandler}  helperText = "Please key in the number of pledges that you think you need to attain before converting it to an actual listing " sx = {{width:  '80%', marginTop: "5%" }} label = "Pledge Target" multiline = {false}> </TextField>
+
+
+                </div>
+
+
+                </div>
+
+                <div  style = {{alignItems: "center", justifyContent: 'center', display: 'flex', marginTop: "3%"}}> 
+                <Button onClick = {uploadOrder} variant = "outlined" sx = {{borderColor: "black", color: "black"}}> Enter </Button>
+                </div>
+
+             
             </div>
 
             
